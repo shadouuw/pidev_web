19cd45bd4909a313edf965ba018e1a8cbcbe4123
abirbhm <78984618+abirbhm@users.noreply.github.com>
2021-04-22 11:17:53 +0200

    Add files via upload

---
+++ Reclamation.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use  Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Reclamation
+ *
+ * @ORM\Table(name="reclamation")
+ * @ORM\Entity
+ */
+class Reclamation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_reclamation", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idReclamation;
+
+    /**
+     * @var string
+     *
+     * @Assert\Length(min=10,minMessage="content length must be greater than 10")
+
+     * @ORM\Column(name="contenu", type="string", length=300, nullable=false)
+     */
+    private $contenu;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_utilisateur", type="integer", nullable=false)
+     */
+    private $idUtilisateur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="telephone", type="integer", nullable=false)
+     */
+    private $telephone;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="etat", type="string", length=50, nullable=true, options={"default"="Non traité"})
+     */
+    private $etat = 'Non traité';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=30, nullable=false)
+     */
+    private $type;
+
+    public function getIdReclamation(): ?int
+    {
+        return $this->idReclamation;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getIdUtilisateur(): ?int
+    {
+        return $this->idUtilisateur;
+    }
+
+    public function setIdUtilisateur(int $idUtilisateur): self
+    {
+        $this->idUtilisateur = $idUtilisateur;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(int $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(?string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+
+}
